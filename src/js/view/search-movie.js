5ab1ddc744a8628a7e4f426c3ab8b4666d6a8ce4
anhTuspham <panhtu0902@gmail.com>
2023-06-16 04:16:55 +0700

    add display movie list, update UI

--- src/js/view/search-movie.js
+++ src/js/view/search-movie.js
@@ -1,10 +1,16 @@
 import {API_URL} from "../config.js";
+
 const dotenv = require('dotenv');
 dotenv.config();
 const apiKey = process.env.OMDB_API_KEY
 
+const overlay = document.querySelector('.overlay')
+const container = document.querySelector('.container')
+const body = document.querySelector('body')
 const movieSearchBox = document.getElementById('search-box');
 const movieSearchList = document.getElementById('search-list-container')
+const closeButton = document.querySelector('.w-6')
+
 
 // Load movie data
 const loadMovieData = async function(searchItem){
@@ -61,17 +67,109 @@ const displayMovieList = function (movie){
             <div class="search-item-info">
                  <h4>${movie[i].Title}</h4>
             </div>`
-
         movieSearchList.appendChild(movieList);
     }
+    loadMovieDetail()
 }
 
 // Load Movie Detail
+let a = 0
 
+const loadMovieDetail = function(){
+    const movieSingle = movieSearchList.querySelectorAll('.search-list')
+    movieSingle.forEach(movie => {
+        movie.addEventListener('click',async () =>{
+            a = 1;
+            console.log(a)
+            console.log(movie.dataset.id)
+            movieSearchList.classList.add('hidden')
+            movieSearchBox.value = '';
+            // container.classList.add('overlay-active');
+            body.classList.add('overlay-active');
+            // overlay.classList.add('overlay-active');
+            const data = await fetch(`${API_URL}?apikey=${apiKey}&i=${movie.dataset.id}`)
+                .then(res => res.json())
+            console.log(data)
+            displayMovieDetail(data)
+
+        })
+    })
+}
+
+// Display movie detail
+const displayMovieDetail = function (movie){
+    let moviePoster;
+    overlay.innerHTML =
+        ` <div class="movie-detail-container">
+            <div class="movie-detail">
+                <div class="left-content">
+                    <div class="movie-image">
+                        <img alt="movie-image" src="${(movie.Poster !== "N/A") ? movie.Poster : "image-not-found.png"}">
+                    </div>
+                </div>
+                <div class="right-content">
+                    <div class="close-detail-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </div>
+                    <div class="movie-information">
+                        <div class="movie-title">
+                            <h2>${movie.Title}</h2>
+                        </div>
+                        <div class="movie-review">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+                            ${movie.imdbRating}
+                        </div>
+                        <div class="movie-description">
+                            ${movie.Plot}
+                        </div>
+                        <div class="movie-information-group">
+                            <div class="detail movie-director">
+                                <h4>Director:</h4>
+                                <h4>${movie.Director}</h4>
+                            </div>
+                            <div class="detail movie-actor">
+                                <h4>Actors:</h4>
+                                <h4>${movie.Actors}</h4>
+                            </div>
+                            <div class="detail movie-genre">
+                                <h4>Tags:</h4>
+                                <h4>${movie.Genre}</h4>
+                            </div>
+                            <div class="detail movie-duration">
+                                <h4>Runtime:</h4>
+                                <h4>${movie.Runtime}</h4>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+            </div>
+        </div>`
+}
+
+// window.addEventListener('click',e =>{
+//     if(a === 1 && e.target.closest('.overlay')){
+//         overlay.classList.remove('overlay-active');
+//         container.classList.remove('overlay-active');
+//         body.classList.remove('overlay-active')
+//         a = 0;
+//         console.log('5')
+//     }
+// })
+
+window.addEventListener('click', e =>{
+    if(e.target.closest('.w-6')){
+        body.classList.remove('overlay-active')
+    }
+})
 
 
 window.addEventListener('click',e =>{
-    if(e.target.closest('.search-container')){
+    if(e.target.closest('.search-box')){
         movieSearchList.classList.remove('hidden')
     }
     else movieSearchList.classList.add('hidden')
