f952ccebfa52d6a019f41e172a5a2750daa440b5
anhTuspham <panhtu0902@gmail.com>
2023-07-03 00:11:48 +0700

    update UI input login, hide/show Log form

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,14 +3,17 @@ import './view/menu-bar.js'
 import './view/category.js'
 import './view/search-movie.js'
 import './view/remind-box.js'
-import './view/log.js'
 import {displayMoviePopular, renderSpinner, searchMovieData} from "./view/search-movie.js";
+import {showPassword, toggleLoginForm} from "./view/log.js";
 
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 const init = function (){
+    toggleLoginForm()
+    showPassword()
     searchMovieData();
     displayMoviePopular();
 }
--- src/js/view/log.js
+++ src/js/view/log.js
@@ -1,11 +1,34 @@
+const body = document.querySelector('body')
 const myPassword = document.getElementById('password')
+const showPasswordCheckbox = document.getElementById('show-password-checkbox')
 
-window.addEventListener('click', (e) => {
-    if(e.target.closest('.show-password')){
-        if(myPassword.type === 'password'){
-            myPassword.type = 'text'
-            console.log(1111)
+const loginBtn = document.getElementById('login-btn');
+const closeLogForm = document.querySelector('.close-log-detail-btn');
+
+export const showPassword = function (){
+    showPasswordCheckbox.checked = !showPasswordCheckbox.checked
+    showPasswordCheckbox.checked = false;
+
+    window.addEventListener('click',e =>{
+        if(e.target.closest('.show-password')){
+            if(e.target.classList.contains('show-password')){
+                showPasswordCheckbox.checked = !showPasswordCheckbox.checked
+            }
+            if(showPasswordCheckbox.checked){
+                myPassword.type = 'text';
+            }
+            else myPassword.type = 'password'
         }
-        else myPassword.type = 'password'
-    }
-})
+    })
+}
+body.classList.add('log-form-active')
+
+export const toggleLoginForm = () =>{
+    loginBtn.addEventListener('click',() =>{
+        body.classList.add('log-form-active')
+    })
+    closeLogForm.addEventListener('click', () =>{
+        body.classList.remove('log-form-active')
+    })
+}
+
