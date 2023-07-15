c967a9e1bfc3e031dfa326e927e9a653b7375880
anhTuspham <panhtu0902@gmail.com>
2023-07-15 23:42:01 +0700

    responsive version 1.1

--- src/js/view/header.js
+++ src/js/view/header.js
@@ -1,62 +1,37 @@
+const container = document.querySelector('.container');
 const searchListContainer = document.querySelector('.search-list-container');
 const searchContainer = document.querySelector('.search-container');
-const desktopBtns = document.querySelectorAll('.desktop-btn');
-const mobileBtns = document.querySelectorAll('.mobile-btn');
-const headerBtnContainer = document.querySelector('.header-btn-container');
 const logo = document.querySelector('.logo-part');
-const searchBtn = document.querySelector('.search-btn');
 const searchMobile = document.getElementById('search-mobile')
 const searchBtnGlass = document.querySelector('.search-btn.glass');
-const searchBtnMicro = document.querySelector('.search-btn.micro')
-const searchInput = document.querySelector('.search__input')
-const showSearchInputMobile = () =>{
-    if(window.screen.width <= 1024) {
-        searchContainer.style.display = 'none';
-        searchMobile.addEventListener('click', () => {
-            searchContainer.style.display = 'block'
-            logo.style.display = 'none';
-            headerBtnContainer.style.display = 'none';
-            searchContainer.style.width = '100%';
-            searchListContainer.style.width = `${searchContainer.offsetWidth}px`;
+const header = document.querySelector('.header')
+const menuMobile = document.getElementById('menu-mobile')
 
-        })
-        searchBtnGlass.addEventListener('click', () => {
-            searchContainer.style.display = 'none';
-            logo.style.display = 'block';
-            headerBtnContainer.style.display = 'flex';
-            searchContainer.style.width = '50%';
-        })
-    }
-    else{
-        searchContainer.style.display = 'block'
-    }
-}
-const switchDesktopMobile = () =>{
-    desktopBtns.forEach(desktopBtn =>{
-        if(window.screen.width < 1024){
-            desktopBtn.style.display = 'none'
-        }
-        else {
-            desktopBtn.style.display = 'block'
-        }
-    })
-    mobileBtns.forEach(mobileBtn =>{
-        if(window.screen.width < 1024){
-            mobileBtn.style.display = 'block'
-        }
-        else {
-            mobileBtn.style.display = 'none'
-        }
-    })
-}
-window.addEventListener('resize', ()=>{
+
+searchMobile.addEventListener('click', () =>{
+    header.classList.add('search-mobile');
+    container.classList.remove('menu-mobile')
     searchListContainer.style.width = `${searchContainer.offsetWidth}px`;
-    switchDesktopMobile()
-    showSearchInputMobile()
+})
+searchBtnGlass.addEventListener('click',() =>{
+    header.classList.remove('search-mobile')
+})
+menuMobile.addEventListener('click',() =>{
+    container.classList.toggle('menu-mobile')
+})
+
+window.addEventListener('resize', ()=>{
+    if(window.screen.width <= 1024){
+        searchContainer.style.width = '100%';
+        searchListContainer.style.width = `${searchContainer.offsetWidth}px`;
+    }
+    else {
+        searchContainer.style.width = '50%';
+        searchListContainer.style.width = `${searchContainer.offsetWidth}px`;
+    }
 })
 searchListContainer.style.width = `${searchContainer.offsetWidth}px`;
-switchDesktopMobile()
-showSearchInputMobile()
+
 
 
 
